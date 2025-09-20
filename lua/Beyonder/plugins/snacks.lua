098899680704380f47a7b971e1d3b335b28e4cbc
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-09-20 21:59:42 +0545

    lazygit logs

--- lua/Beyonder/plugins/snacks.lua
+++ lua/Beyonder/plugins/snacks.lua
@@ -8,5 +8,6 @@ return{
   },
   keys = {
       {"<leader>lg", function() require("snacks").lazygit() end, desc = "Lazygit"},
+      {"<leader>gl", function() require("snacks").lazygit.log() end, desc = 'Lazygit Logs'}
   },
 }
