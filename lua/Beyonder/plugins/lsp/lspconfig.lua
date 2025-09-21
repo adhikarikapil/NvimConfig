cbb03352b80105e4f0f9ef9c336036863869aa24
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-09-21 21:31:48 +0545

    markdown lint formatting

--- lua/Beyonder/plugins/lsp/lspconfig.lua
+++ lua/Beyonder/plugins/lsp/lspconfig.lua
@@ -235,6 +235,23 @@ return {
 			local conform = require("conform")
 
 			conform.setup({
+                 ["markdown-toc"] = {
+                    condition = function(_, ctx)
+                        for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+                            if line:find("<!%-%- toc %-%->") then
+                                return true
+                            end
+                        end
+                    end,
+                },
+                ["markdownlint-cli2"] = {
+                    condition = function(_, ctx)
+                        local diag = vim.tbl_filter(function(d)
+                            return d.source == "markdownlint"
+                        end, vim.diagnostic.get(ctx.buf))
+                        return #diag > 0
+                    end,
+                },
 				formatters_by_ft = {
 					lua = { "stylua" },
 					javascript = { "prettier" },
