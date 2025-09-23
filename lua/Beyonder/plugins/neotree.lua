a05a1a549e0348ef0c29b12bc67f7670db1c4211
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-09-23 16:38:25 +0545

    neotree
    Removed mini-files explorer and added neo-tree.

---
+++ lua/Beyonder/plugins/neotree.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        vim.keymap.set('n', "<leader>fa", ':Neotree toggle<CR>')
+        require('neo-tree').setup({
+            window = {
+                width = 50,
+            }
+        })
+    end,
+    lazy = false,
+}
