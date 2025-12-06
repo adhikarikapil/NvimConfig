7344212924e73bc780d4b03c06f3b7b8c1cb09db
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-12-07 02:38:13 +0545

    few tweaks

--- lua/Beyonder/plugins/dadbod.lua
+++ lua/Beyonder/plugins/dadbod.lua
@@ -1,26 +1,38 @@
 return {
-	{
-		"tpope/vim-dadbod",
-	},
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		keys = {
-			{ "<leader>db", "<cmd>DBUIToggle<CR>", desc = " Open DB UI " },
-			{ "<leader>dc", "<cmd>DBUIClose<CR>", desc = "Close DB UI" },
-			{ "<leader>dq", "<cmd>DBUIFindBuffer<CR>", desc = "Find DB Buffer" },
-		},
-		config = function()
-			vim.g.db_ui_use_nerd_fonts = 1
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "dbout",
-				callback = function()
-					vim.cmd("wincmd L")
-					vim.cmd("vertical resize 85")
-				end,
-			})
-		end,
-	},
-	{
-		"kristijanhusak/vim-dadbod-completion",
-	},
+    {
+        "tpope/vim-dadbod",
+    },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        keys = {
+            { "<leader>db", "<cmd>DBUIToggle<CR>", desc = "Open DB UI" },
+            { "<leader>dc", "<cmd>DBUIClose<CR>", desc = "Close DB UI" },
+            { "<leader>dq", "<cmd>DBUIFindBuffer<CR>", desc = "Find DB Buffer" },
+        },
+
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+            vim.g.db_ui_winwidth = 20
+        end,
+
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "dbout",
+                callback = function()
+                    -- disable folding so results aren't collapsed
+                    vim.opt_local.foldenable = false
+                    vim.opt_local.foldcolumn = "0"
+                    vim.opt_local.foldmethod = "manual"
+
+                    -- open results on the right
+                    vim.cmd("wincmd L")
+                    vim.cmd("vertical resize 110")
+                end,
+            })
+        end,
+    },
+    {
+        "kristijanhusak/vim-dadbod-completion",
+    },
 }
+
